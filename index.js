c99604be4db4986b3e764e0e02b46727759460e2
Rince Mathew <rincemathewmanattu@gmail.com>
2023-07-10 19:47:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import the express function
+const express = require('express')
+const app = express()
+
+// const router = express.Router()
+
+
+
+
+
+
+// Start the server
+const port = 3000
+
+
+
