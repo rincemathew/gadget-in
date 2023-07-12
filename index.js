5a0a0d6f861b46d55e625e1b39f488302816ea75
Rince Mathew <rincemathewmanattu@gmail.com>
2023-07-12 19:56:56 +0530

    env and vhost set

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 const express = require('express')
 const app = express()
 
-// const router = express.Router()
+//loading environment variables
+require('dotenv').config()
 
+//importing vhost for subdomin configuration
+const vhost = require('vhost')
 
+//importing two router options user, admin
+const userRouter = require('./routers/userRoute')
+const adminRouter = require('./routers/adminRoute')
 
-
+//used vhost to create subdomains
+app.use(vhost(process.env.DOMAIN_NAME, userRouter));
+app.use(vhost(`admin.${process.env.DOMAIN_NAME}`, adminRouter));
 
 
 // Start the server
-const port = 3000
+app.listen(process.env.PORT,()=>console.log("Server Started"))
 
 
 
