a0e3ce9a1d68e6f8820ecf0a80e84b492b09a4fa
Rince Mathew <rincemathewmanattu@gmail.com>
2023-07-19 16:24:33 +0530

    user design

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+
+
+
+const home_page = async(req,res) => {
+    res.render('user/index',{message: "",})
+}
+
+
+module.exports = {home_page,
+    }
--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/userController')
 
 
-router.get('/', (req, res) => {
-    res.send('Hello World! User')
-  })
+//// LOGIN AND REGISTER
+
+
+////VIEWS
+router.get('/',userController.home_page)
 
 module.exports = router;
