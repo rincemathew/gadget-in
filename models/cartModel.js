25d51346ec1f75e7822253cb3d3386186f7aa32e
Rince Mathew <rincemathewmanattu@gmail.com>
2023-07-29 14:27:15 +0530

    admin template using

---
+++ models/cartModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const cartSchema = mongoose.Schema({
+    userid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    products:[
+    {
+      productid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Products",
+        required:true,
+      },
+      quantity:{
+        type:Number,
+        required:true
+      },
+    }
+    ]
+},{versionkey:false})
+
+module.exports = mongoose.model('cart-details',cartSchema)
